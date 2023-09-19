79e60a45618243fa8ad7ded92e84f0c78a69c6c0
raaf17 <raafimuhammadd123@gmail.com>
2023-09-19 10:33:51 +0700

    belajar useState

---
+++ src/components/introduction.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function Introduction({name}) {
+  return (
+    <div>
+      <h1>Introduction</h1>
+      <p>Halo nama saya adalah: {name}</p>
+    </div>
+  );
+}
+
+export default Introduction;
